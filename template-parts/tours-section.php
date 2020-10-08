8af2067ffc64fb3b55595b223bc86debc3938555
ruvick <rudikov.web@ya.ru>
2020-10-08 23:57:29 +0300

    Изменил город

--- template-parts/tours-section.php
+++ template-parts/tours-section.php
@@ -16,8 +16,9 @@
 				<a href="<?php echo get_permalink(8)?>" class="light-button light-button__inverse">Подробнее</a>
 			</div>
 			<div class="tours-item">
-				<a href="<?php echo get_permalink(6);?>" class="tours-item__title" style="background-image: url(<?php echo get_template_directory_uri();?>/img/monten.jpg);">
-					<h3 class="tours-title">Черногория</h3>
+				<a href="<?php echo get_permalink(6);?>" class="tours-item__title" 
+					style="background-image: url(<?php echo get_template_directory_uri();?>/img/monten.jpg);">
+					<h3 class="tours-title">Курск</h3>
 				</a>
 				<a href="<?php echo get_permalink(6);?>" class="tours-item__subtitle">
 					<h3 class="tours-subtitle">Тур «Апгрейд»</h3>
